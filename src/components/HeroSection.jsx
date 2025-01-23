05d4448ec5966354032fc5b987c896263ddd642c
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2025-01-23 03:48:02 +0100

    Add hover effect

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
   return (
-    <section className="relative bg-cover bg-center h-96 flex items-center justify-center text-white mt-1 px-4 sm:px-6 md:px-8 lg:px-10" style={{ backgroundImage: 'url(/Hero.jpg)' }}>
+    <section className="relative bg-cover bg-center h-[550px] flex items-center justify-center text-white mt-1 px-4 sm:px-6 md:px-8 lg:px-10" style={{ backgroundImage: 'url(/Hero.jpg)' }}>
       {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
@@ -16,7 +16,8 @@ const HeroSection = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, ease: "easeOut" }}
         >
-          Fly in Style This <span className="text-[#2e5c99]">Detty December</span> – Affordable Luxury Awaits!
+          Fly in Style This Detty December Affordable Luxury Awaits!  
+          {/*  <span className="text-[#2e5c99]"></span> */}
         </motion.h1>
 
         {/* Subheadline */}
